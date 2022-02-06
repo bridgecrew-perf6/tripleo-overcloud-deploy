4a62703d0661fbbcd6122342da279dd2a77697bb
Ram Meena <ram.meena.mail@gmail.com>
2022-02-06 11:01:53 +0000

    baremetal playbook execution

--- provision-nodes.sh
+++ provision-nodes.sh
@@ -1,3 +1,5 @@
+#--output ~/tripleo-overcloud-deploy/environments/overcloud-provisioned-nodes.yaml \
+#~/tripleo-overcloud-deploy/templates/overcloud_node_provision.yaml
 openstack overcloud node provision \
 --stack overcloud \
 --network-config \
